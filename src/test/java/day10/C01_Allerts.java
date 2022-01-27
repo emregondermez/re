ab1afedbc4cb22b0b1934d420c8b0738bb77df40
talfik2 <emregondermez@gmail.com>
2022-01-27 22:58:55 +0100

    27.01.2022

---
+++ src/test/java/day10/C01_Allerts.java
@@ -0,0 +1,90 @@
+package day10;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class C01_Allerts {
+
+    //● Bir class olusturun: Alerts
+    //● https://the-internet.herokuapp.com/javascript_alerts adresine gidin.
+    //● Bir metod olusturun: acceptAlert
+    //        ○ 1. butona tıklayın, uyarıdaki OK butonuna tıklayın ve result mesajının
+    //        “You successfully clicked an alert” oldugunu test edin.
+    //● Bir metod olusturun: dismissAlert
+    //        ○ 2. butona tıklayın, uyarıdaki Cancel butonuna tıklayın ve result mesajının
+    //        “successfuly” icermedigini test edin.
+    //● Bir metod olusturun: sendKeysAlert
+    //        ○ 3. butona tıklayın, uyarıdaki metin kutusuna isminizi yazin, OK butonuna     tıklayın
+    //        ve result mesajında isminizin görüntülendiğini doğrulayın.
+
+WebDriver driver;
+    @BeforeClass
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+    }
+
+
+    @Test
+    public void acceptAlert() {
+       // Bir metod olusturun: acceptAlert
+        //        ○ 1. butona tıklayın, uyarıdaki OK butonuna tıklayın ve result mesajının
+        //        “You successfully clicked an alert” oldugunu test edin.
+        driver.findElement(By.xpath("//button[@onclick='jsAlert()']")).click();
+        driver.switchTo().alert().accept();
+
+        WebElement result = driver.findElement(By.xpath("//p[@id='result']"));
+        String actualResult =result.getText();
+        String expectedResult = "You successfully clicked an alert";
+
+        Assert.assertEquals(actualResult,expectedResult,"the result didn't match");
+
+
+    }
+    @Test
+    public void dismissAlert() {
+    //Bir metod olusturun: dismissAlert
+    //  ○ 2. butona tıklayın, uyarıdaki Cancel butonuna tıklayın ve result mesajının
+    //“successfully” icermedigini test edin.
+
+        driver.findElement(By.xpath("//button[@onclick='jsConfirm()']")).click();
+        driver.switchTo().alert().accept();
+        WebElement result = driver.findElement(By.xpath("//p[@id='result']"));
+        String actualResult =result.getText();
+        String expectedResult = "successfully";
+        Assert.assertTrue(actualResult.contains(expectedResult));
+    }
+    @Test
+    public void sendKeysAlert() {
+        //Bir metod olusturun: sendKeysAlert
+        //   ○ 3. butona tıklayın, uyarıdaki metin kutusuna isminizi yazin, OK butonuna  tıklayın
+        //  ve result mesajında isminizin görüntülendiğini doğrulayın.
+        driver.findElement(By.xpath("//button[@onclick='jsPrompt()']")).click();
+        driver.switchTo().alert().sendKeys("Emre");
+        driver.switchTo().alert().accept();
+        WebElement result = driver.findElement(By.xpath("//p[@id='result']"));
+        String actualAlertString= result.getText();
+        String expectedAlertString ="Emre";
+
+      Assert.assertTrue(actualAlertString.contains(expectedAlertString));
+
+    }
+
+    @AfterClass
+    public void teardown() {
+        // driver.close();
+    }
+}

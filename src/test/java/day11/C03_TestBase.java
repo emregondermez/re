93e02f81d2daa7e6e1452f00b1de8dde92e166bb
talfik2 <emregondermez@gmail.com>
2022-01-29 15:49:39 +0100

    29.01.2022

--- src/test/java/day11/C03_TestBase.java
+++ src/test/java/day11/C03_TestBase.java
@@ -1,4 +1,13 @@
 package day11;
 
-public class C03_TestBase {
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class C03_TestBase extends TestBase {
+
+
+    @Test
+    public void test01() {
+        driver.get("https://www.amazon.com");
+    }
 }

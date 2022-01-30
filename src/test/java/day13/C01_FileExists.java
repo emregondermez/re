846351861551626b82fa0936da50514100c89f26
talfik2 <emregondermez@gmail.com>
2022-01-31 00:19:29 +0100

    31.01.2022

---
+++ src/test/java/day13/C01_FileExists.java
@@ -0,0 +1,14 @@
+package day13;
+
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class C01_FileExists {
+
+
+    @Test
+    public void test01() {
+        System.out.println(System.getProperty("user.home"));
+    }
+
+}

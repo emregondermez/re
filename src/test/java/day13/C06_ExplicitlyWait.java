2b043da3ce1e0b6c804b23e71f8dc98c91fd7686
talfik2 <emregondermez@gmail.com>
2022-02-01 13:08:18 +0100

    01.02.2022

---
+++ src/test/java/day13/C06_ExplicitlyWait.java
@@ -0,0 +1,17 @@
+package day13;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+import java.time.Duration;
+
+public class C06_ExplicitlyWait extends TestBase {
+
+
+    @Test
+    public void wait1() {
+    //    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
+    }
+}
